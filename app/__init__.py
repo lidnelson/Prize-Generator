6fc1e8deccac88369e66c3f4cd1377d0653c212e
solomonbada <solomonbada@hotmail.com>
2019-11-12 11:04:48 +0000

    added create.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY']='f9bf78bavdwvwefveavecd2b0b86df9da'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/Prizes'
 
 db = SQLAlchemy(app)
+
 
 
 from app import routes
---
+++ create.py
@@ -0,0 +1,4 @@
+from app import db
+from app.models import Prizes
+
+db.create_all()
