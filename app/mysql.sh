e5d426fd1ff166bad8a1fea8975967ba782f3020
lidnelson <55387227+lidnelson@users.noreply.github.com>
2019-11-14 11:35:20 +0000

    mysql.sh as a start up download in an ec2 instance

---
+++ app/mysql.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+sudo yum update -y
+sudo yum install mysql -y
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo usermod -aG docker ec2-user
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+sudo chmod +x /usr/local/bin/docker-compose
+sudo yum install git
+git clone -b solomon https://github.com/lidnelson/Prize-Generator
+cd Prize-Generator
+sudo docker-compose up -d --build
+
