66b07047e69553673c74914149ad6f22f1b94e8a
EC2 Default User <ec2-user@ip-172-31-8-223.eu-west-1.compute.internal>
2019-11-19 11:28:45 +0000

    added config files

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from app import config
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY']='f9bf78bavdwvwefveavecd2b0b86df9da'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sqldatabase:sqldatabase@sqldatabase.calytdhddf4j.eu-west-1.rds.amazonaws.com/Prizes'
+app.config['SECRET_KEY']= config.secret_key
+app.config['SQLALCHEMY_DATABASE_URI'] = config.database_uri
 
 db = SQLAlchemy(app)
 
