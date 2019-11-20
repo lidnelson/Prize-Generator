b99cadece658fd13e98d1c36e8a2b8425f8a8eba
solomonbada <55092939+solomonbada@users.noreply.github.com>
2019-11-20 22:43:19 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY']='f9bf78bavdwvwefveavecd2b0b86df9da'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY']=''
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
 db = SQLAlchemy(app)
 
