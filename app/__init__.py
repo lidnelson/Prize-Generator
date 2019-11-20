675d5995b3e1091f75b0cd91c3dbd227677e1c07
EC2 Default User <ec2-user@ip-172-31-8-223.eu-west-1.compute.internal>
2019-11-20 09:37:13 +0000

    removed unneccesary documents

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app import config
 
+
 app = Flask(__name__)
 
-app.config['SECRET_KEY']= config.secret_key
+app.config['SECRET_KEY'] = config.secret_key
 app.config['SQLALCHEMY_DATABASE_URI'] = config.database_uri
 
 db = SQLAlchemy(app)
